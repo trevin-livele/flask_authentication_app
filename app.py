cefc68d6ecddf18c4758fdae9a72050ee03d3607
trevin-livele <liveletrevin6@gmail.com>
2022-02-06 21:34:13 +0300

    added login

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #app.py
 
 from crypt import methods
+from distutils.log import debug
 from myproject import app,db
 from flask import render_template,redirect,request,url_for,flash,abort
 from flask_login import login_user,login_required,logout_user
+from myproject import forms
 from myproject.models import User
 from myproject.forms import LoginForm,RegistrationForm
 
@@ -34,11 +36,45 @@ def logout():
 @app.route("/login",methods=['GET','POST'])
 def login():
 
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+
+            login_user(user)
+            flash('Logged in Succesfully!')
+
+            next = request.args.get('next')
+
+            if next == None or not next[0] == '/':
+                next = url_for('welcome_user')
+
+            return redirect(next)
+    return render_template('login.html',form=form)
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                username=form.username.data,
+                password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash("Thanks for registration!")
+        return redirect(url_for('login'))
+    return render_template('register.html',form=form)
 
 
 
-@app.route("/login",methods=['GET','POST'])
-def login():
+if __name__== '__main__':
+    app.run(debug=True)
 
-    pass
+
+
+
+
